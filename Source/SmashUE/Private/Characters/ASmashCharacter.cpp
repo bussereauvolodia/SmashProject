5dc068bca7cb1f4592f73503d6e15c6f76f3a1f1
Volod <volodiabussereau@gmail.com>
2024-09-17 16:16:26 +0200

    Base BP creations

---
+++ Source/SmashUE/Private/Characters/ASmashCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/ASmashCharacter.h"
+
+
+// Sets default values
+AASmashCharacter::AASmashCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AASmashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AASmashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AASmashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
